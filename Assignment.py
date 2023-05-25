2d566f45c127af9b111c621af9d8726b6b8250fe
prashannakharel <prashanna.kharel@outlook.com>
2023-05-24 19:29:07 -0500

    Create Assignment.py

---
+++ Assignment.py
@@ -0,0 +1,7 @@
+x = "This assignment was done on Google Colab, an alternative for Binder"
+y = "We always complete our assignment on time."
+z = "What's stopping the professor to give this assignment full credits?"
+
+print(x)
+print(y)
+print(z)
